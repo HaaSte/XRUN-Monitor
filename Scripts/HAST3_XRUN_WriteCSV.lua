f59b7d0fd8c64a8ea8b8b6136cd29db54731d024
HaaSte <67197905+HaaSte@users.noreply.github.com>
2026-02-11 16:19:45 +0100

    Refactor console output in WriteCSV.lua
    Removed console messages for TimeOS, TimeAudio, and Length. Added a summary message for XRUNs written.

--- Scripts/HAST3_XRUN_WriteCSV.lua
+++ Scripts/HAST3_XRUN_WriteCSV.lua
@@ -30,11 +30,10 @@ while reaper.gmem_read(i) == 42 do
   local len = reaper.gmem_read(i+3)
     
   f:write(string.format("%.0f,%.1f,%.0f,%.1f\n", no, time_os, time_audio, len))
-  reaper.ShowConsoleMsg(string.format("TimeOS: %.1f\n", time_os*srate))
-  reaper.ShowConsoleMsg(string.format("TimeAudio: %.0f\n", time_audio*srate))
-  reaper.ShowConsoleMsg(string.format("Length: %.1f\n\n", len*srate))
   no = no + 1
   i = i + 4
 end
 
 f:close()
+
+reaper.ShowConsoleMsg(no-1 .. " XRUNs written to " .. fname)
